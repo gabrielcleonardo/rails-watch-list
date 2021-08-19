50c9a6f10457ab1b240c06441966578ca09854e7
Gabriel Leonardo <gabrielcleonardo@hotmail.com>
2021-08-19 15:08:46 -0300

    validates ok

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie_id, uniqueness: { scope: :list_id }
+
+  validates :comment, length: { minimum: 5 }
 end
